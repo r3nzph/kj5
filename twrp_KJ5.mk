92645ffab1381e0d3fa90333a48d642a64d49891
r3nzph <abarracosorenznino@gmail.com>
2024-09-02 20:23:02 +0800

    Update twrp_KJ5.mk

--- twrp_KJ5.mk
+++ twrp_KJ5.mk
@@ -31,7 +31,6 @@ $(call inherit-product, device/tecno/KJ5/device.mk)
 # Inherit some common TWRP stuff
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := KJ5
 PRODUCT_NAME := twrp_KJ5
@@ -39,7 +38,4 @@ PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO KJ5
 PRODUCT_MANUFACTURER := tecno
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-PRIVATE_BUILD_DESC="vnd_kj5_h6931-user 12 SP1A.210812.016 581605 release-keys"
-
-BUILD_FINGERPRINT := TECNO/KJ5-OP/TECNO-KJ5:12/SP1A.210812.016/240411V1162:user/release-keys
+PRODUCT_GMS_CLIENTID_BASE := android-tecno
