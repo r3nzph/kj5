48d8a3251e1a2ecbb29aeae3a5dd651b13b8dcf6
r3nzph <abarracosorenznino@gmail.com>
2024-09-05 08:18:59 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -104,6 +104,3 @@ TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
 # Set build date to Jan 1 2009 00:00:00
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.date.utc=1230768000
-
-#TW_OVERRIDE_SYSTEM_PROPS := \
-    "ro.build.product;ro.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
